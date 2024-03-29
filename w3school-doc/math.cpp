fd0fcf110fda5473cc47d75d44cf4f9ac95247c6
raihanuldev <rihanthedev@gmail.com>
2024-03-29 12:40:51 +0600

    switch

---
+++ w3school-doc/math.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    cout << sqrt(70);
+    cout << round(79);
+    cout << log(39);
+    return 0;
+}
---
+++ w3school-doc/switch.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int student_roll = 789145;
+    switch (student_roll)
+    {
+    case 789144:
+        cout << "Student Name: Raihanul Islam \n Department: CSE";
+        break;
+    case 789145:
+        cout << "Student Name : Sanbin sowkot Sahel \n Department : CSE";
+        break;
+    case 789146:
+        cout << "Student Name: Sumit Barua \n Department: CSE";
+    case 789147:
+        cout << "Student Name: Sayed Habib Ullah \n Department: CSE";
+    case 789156:
+        cout << "Student Name: Raihan Alom CR BIchi 6 ta \n Department: CSE";
+    case 789160:
+        cout << "Student Name: Abir  CR BIchi 16 ta \n Department: CSE";
+    }
+}
