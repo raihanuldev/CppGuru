4cc44d9740280381e83994a41e1e8f3aca1c4d0d
raihanuldev <rihanthedev@gmail.com>
2024-03-29 12:25:55 +0600

    env added

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World ";
+    int age;
+    cout << "What is Your Age: ";
+    cin >> age;
+    cout << "Your age is " << age;
+    return 0;
+}
