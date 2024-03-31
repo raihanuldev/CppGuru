bdcebc57fc6e28bef81145f1f78837d937c7c642
raihanuldev <rihanthedev@gmail.com>
2024-03-31 12:44:03 +0600

    array

---
+++ INFO.md
@@ -0,0 +1,1 @@
+* g++ array.cpp -o array 
---
+++ w3school-doc/array.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string cars[3] = {"bmw","CMW","HBW"};
+    int num[2] = {1,2};
+    cout <<cars[0];
+    cout <<num[1];
+}
