aee4830ae968e6944cb7336221f5d4ae7ec4206c
raihanuldev <rihanthedev@gmail.com>
2024-03-31 12:48:49 +0600

    array-loop practice

---
+++ w3school-doc/array-loop.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string cars[5] = {"BMW", "ABW", "CBW", "XBW", "YGD"};
+    for (int i = 0; i < 5; i++)
+    {
+        cout << cars[i] <<'\n';
+    };
+}
