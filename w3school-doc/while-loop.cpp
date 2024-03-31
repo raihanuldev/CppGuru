019e828be3845bf5d87a597be9990d8f1d924dad
raihanuldev <rihanthedev@gmail.com>
2024-03-31 13:04:32 +0600

    while loop

--- w3school-doc/while-loop.cpp
+++ w3school-doc/while-loop.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main()
 {
     int board_roll = 789144;
+    int rolls = {789144,789145,789146,789147}
     while (board_roll < 789190)
     {
         cout << board_roll << '\n';
