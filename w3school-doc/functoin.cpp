59a69ec9e2bde3f7322e00efa5139c91812591dc
raihanuldev <rihanthedev@gmail.com>
2024-04-05 12:09:02 +0600

    function

---
+++ w3school-doc/functoin.cpp
@@ -0,0 +1,16 @@
+#include <iostream>;
+using namespace std;
+
+
+void myfristFunction(){
+    int age;
+    cout <<"Hey what is your age: ";
+    cin >> age;
+    cout << "Your age is " << age;
+
+}
+
+int main(){
+    myfristFunction();
+    return 0;
+}
